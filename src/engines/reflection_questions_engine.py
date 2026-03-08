7225bee4c7f21ea3f5695a918ff6749969ae367e
Gourab Nath <gourabnath88@gmail.com>
2026-03-08 22:26:25 +0530

    added function descriptions

--- src/engines/reflection_questions_engine.py
+++ src/engines/reflection_questions_engine.py
@@ -4,6 +4,12 @@ from src.llm_client import generate_response
 
 
 def reflection_questions_generate(story: str) -> str:
+    '''
+    Description: this function generated around 3-5 reflecting questions related to the problem of interest.
+    Input: the function takes one input,
+        - story: a story related to the problem of interest (str)
+    Output: 3-5 reflecting questions as per the instructions given in the system prompt.
+    '''
     messages = [
       {"role": "system", "content": system_prompt},
       {"role": "user", "content": build_user_prompt(story)}
