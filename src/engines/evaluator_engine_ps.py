bf5874bc113342ac3b27f594b190b260873da193
Gourab Nath <gourabnath88@gmail.com>
2026-03-08 22:31:30 +0530

    added problem descriptions

--- src/engines/evaluator_engine_ps.py
+++ src/engines/evaluator_engine_ps.py
@@ -5,10 +5,10 @@ from src.llm_client import generate_response
 def problem_statement_evaluator(problem_statement, qa_discussion, story):
   '''
   Descriiption: This function evaluated the user-defined analytical problem statement based on a list of criteria defined in the system prompt.
-  Input: 
-    problem_statement: user-defined problem statement (string).
-    qa_discussion: a dictionary containing the q&a discussions related to the problem statement (dictionary).
-    story: the story related to the problem (string).
+  Input: this function takes 3 inputs,
+    - problem_statement: user-defined problem statement (string).
+    - qa_discussion: a dictionary containing the q&a discussions related to the problem statement (dictionary).
+    - story: the story related to the problem (string).
   Output: problem evaluation and refined problem statement (string).
 
   '''
