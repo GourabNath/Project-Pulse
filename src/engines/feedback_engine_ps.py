7e63bc2c1862a8ae08f9b202600f294c6f6bf9e9
Gourab Nath <gourabnath88@gmail.com>
2026-03-08 22:21:32 +0530

    added function description

--- src/engines/feedback_engine_ps.py
+++ src/engines/feedback_engine_ps.py
@@ -2,7 +2,14 @@ from src.prompts.feedback_and_finetune import build_system_prompt, build_user_pr
 from src.llm_client import generate_response
 
 def feedback_generator_ps(problem_context, question, answer) -> str:
-
+  '''
+  Description: This function evaluated user response for a reflecting question.
+  Input: 
+    problem_context: a story related to the problem (str).
+    question: a system generated reflecting question related to the problem (str).
+    answer: the answer entered by the user corresponding to the question (str).
+  Output: evaluation of the answer and finetuned response (str).
+  '''
   messages = [
       {"role": "system", "content": build_system_prompt(problem_context, question)},
       {"role": "user", "content": build_user_prompt(answer)}
