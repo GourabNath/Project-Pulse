7cb92494a15d6cd87f2490f6e95109e4942ce0ef
Gourab Nath <gourabnath88@gmail.com>
2026-03-08 22:30:29 +0530

    added problem description

--- src/engines/story_engine.py
+++ src/engines/story_engine.py
@@ -3,6 +3,13 @@ from src.llm_client import generate_response
 
 
 def story_generator(problem_description: str) -> str:
+    '''
+    Description: this function helps in generating a fictitious user story for a given problem description
+    Input: the function takes only one input
+        - problem_description: a small introduction or description of the problem of interest (str)
+    Output: a generated story  for the problem of interest (str).
+
+    '''
     messages = [
         {"role": "system", "content": SYSTEM_PROMPT},
         {"role": "user", "content": build_user_prompt(problem_description)}
