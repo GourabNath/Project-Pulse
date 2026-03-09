4198e8588e1dec56d659172a0045b4779c886ee9
Gourab Nath <gourabnath88@gmail.com>
2026-03-09 23:35:04 +0530

    changed source of llm_client

--- src/engines/feedback_engine_ps.py
+++ src/engines/feedback_engine_ps.py
@@ -1,5 +1,5 @@
 from src.prompts.feedback_and_finetune import build_system_prompt, build_user_prompt
-from src.llm_client import generate_response
+from src.core.llm_client import generate_response
 
 def feedback_generator_ps(problem_context, question, answer) -> str:
   '''
