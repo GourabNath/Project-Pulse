58d94309d20c481bd73172c9d14646b40564f191
Gourab Nath <gourabnath88@gmail.com>
2026-03-10 19:30:23 +0000

    updated variable_identification_controller.py

--- src/controllers/variable_identification_controller.py
+++ src/controllers/variable_identification_controller.py
@@ -1,4 +1,3 @@
-
 def variable_identification(variable_df):
   import time
   for i in range(1,13):
@@ -27,9 +26,12 @@ def variable_identification(variable_df):
       print("\n\n")
       #time.sleep(1)
     
-    #Examples of continuous data types
-    if i == 4:
-      stream_markdown("*Similarly, you won't find it difficult to identify that the following variables are continious:*")
+    #Examples:
+    else:
+      variable_type = variable_df.loc[variable_df.useExample == i, "variable_type"].reset_index(drop=True)
+      variable_type = variable_type[0].lower()
+      string = "Similarly, you won't find it difficult to identify that the following variable is also %s in nature" %variable_type
+      stream_markdown("*"+ string+"*")
       print("\n")
       df_subset = variable_df.loc[variable_df.useExample == i, ["variable", "variable_description", "useExample"]]
       df_subset = df_subset[~df_subset["useExample"].isin([1,2,3])].reset_index(drop=True)
@@ -40,47 +42,4 @@ def variable_identification(variable_df):
           stream_markdown("\n" + "-"*80 + "\n")
       print("\n\n")
 
-
-    #Examples of discrete data types
-    if i == 6:
-      stream_markdown("*Similarly, you won't find it difficult to identify that the following variables are discrete:*")
-      print("\n")
-      df_subset = variable_df.loc[variable_df.useExample == i, ["variable", "variable_description", "useExample"]]
-      df_subset = df_subset[~df_subset["useExample"].isin([1,2,3,4,5])].reset_index(drop=True)
-      for index in range(0, len(df_subset)):
-          stream_markdown("**"+ df_subset.loc[index, "variable"] +"**")
-          variable_description = df_subset.loc[index, "variable_description"]
-          stream_markdown(variable_description)
-          stream_markdown("\n" + "-"*80 + "\n")
-      print("\n\n")
-
-    
-    #Examples of Nominal data types
-    if i == 10:
-      stream_markdown("*Similarly, you won't find it difficult to identify that the following variables are nominal:*")
-      print("\n")
-      df_subset = variable_df.loc[variable_df.useExample == i, ["variable", "variable_description", "useExample"]]
-      df_subset = df_subset[~df_subset["useExample"].isin([1,2,3,4,5,7,8,9])].reset_index(drop=True)
-      for index in range(0, len(df_subset)):
-          stream_markdown("**"+ df_subset.loc[index, "variable"] +"**")
-          variable_description = df_subset.loc[index, "variable_description"]
-          stream_markdown(variable_description)
-          stream_markdown("\n" + "-"*80 + "\n")
-      print("\n\n")
-
-
-    #Examples of ordinal data types
-    if i == 12:
-      stream_markdown("*Similarly, you won't find it difficult to identify that the following variables are ordinal:*")
-      print("\n")
-      df_subset = variable_df.loc[variable_df.useExample == i, ["variable", "variable_description", "useExample"]]
-      df_subset = df_subset[~df_subset["useExample"].isin([1,2,3,4,5,7,8,9,11])].reset_index(drop=True)
-      for index in range(0, len(df_subset)):
-          stream_markdown("**"+ df_subset.loc[index, "variable"] +"**")
-          variable_description = df_subset.loc[index, "variable_description"]
-          stream_markdown(variable_description)
-          stream_markdown("\n" + "-"*80 + "\n")
-      print("\n\n")
-
-    
     
