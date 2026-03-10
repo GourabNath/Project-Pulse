811e662da70b17d091a8d1e8cadc7c44a77a0634
Gourab Nath <gourabnath88@gmail.com>
2026-03-11 01:33:27 +0530

    Update print_multiple_lines.py

--- src/engines/utils/print_multiple_lines.py
+++ src/engines/utils/print_multiple_lines.py
@@ -1,5 +1,5 @@
 
+from src.engines.utils.typewriter import stream_markdown
 def print_multiple_lines(texts):
-    from src.engines.utils.typewriter import stream_markdown
     for t in texts:
       stream_markdown(t)
